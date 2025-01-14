2d744670311569632172bf7dd2e2f8e2bcbc1429
Jordan Masone <jordanmasone1@gmail.com>
2025-01-14 16:04:27 -0500

    Pushed with test changes

--- src/main.js
+++ src/main.js
@@ -17,8 +17,8 @@ let dealerHand = [];
 
 // Create the required amount of playing cards.
 for (let i = 0; i < numOfDecks; i++) {
-	cards.forEach((card) => {
-		suits.forEach((suit) => {
+	suits.forEach((suit) => {
+		cards.forEach((card) => {
 			playingCards.push({
 				card,
 				suit
@@ -29,15 +29,15 @@ for (let i = 0; i < numOfDecks; i++) {
 
 // Fisher-Yates Algorithim for Shuffling
 const shuffle = (cards) => {
-	// for (let i = cards.length - 1; i > 0; i--) {
-	// 	// Generate a random index from 0 to i.
-	// 	const randomIndex = Math.floor(Math.random() * (i + 1));
+	for (let i = cards.length - 1; i > 0; i--) {
+		// Generate a random index from 0 to i.
+		const randomIndex = Math.floor(Math.random() * (i + 1));
 
-	// 	// Swap elements at i and randomIndex.
-	// 	[cards[i], cards[randomIndex]] = [cards[randomIndex], cards[i]];
-	// }
+		// Swap elements at i and randomIndex.
+		[cards[i], cards[randomIndex]] = [cards[randomIndex], cards[i]];
+	}
 
-	return cards.reverse();
+	return cards;
 };
 
 const delay = (ms) => {
