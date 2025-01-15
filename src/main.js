e4b8e16b217d4e1fd9c677abea619e0e75a99e5e
Jordan Masone <jordanmasone1@gmail.com>
2025-01-15 15:08:00 -0500

    Added end game and restart functionality

--- src/main.js
+++ src/main.js
@@ -13,24 +13,32 @@ const playerScoreDiv = document.getElementById('player-score');
 const buttonsDiv = document.getElementById('buttons');
 const hitButton = document.getElementById('hit');
 const standButton = document.getElementById('stand');
+const modal = new bootstrap.Modal(document.getElementById('staticBackdrop'));
+const modalHeaderH1 = document.getElementById('modal-header');
+const modalBodyDiv = document.getElementById('modal-body');
+const modalButton = document.getElementById('modal-button');
 
-let playingCards = [];
+let playingCards;
 let cardIndex = 0;
 
 let playerHand = [];
 let dealerHand = [];
 
 // Create the required amount of playing cards.
-for (let i = 0; i < numOfDecks; i++) {
-	suits.forEach((suit) => {
-		cards.forEach((card) => {
-			playingCards.push({
-				card,
-				suit
+const createDeck = () => {
+	playingCards = [];
+
+	for (let i = 0; i < numOfDecks; i++) {
+		suits.forEach((suit) => {
+			cards.forEach((card) => {
+				playingCards.push({
+					card,
+					suit
+				});
 			});
 		});
-	});
-}
+	}
+};
 
 // Fisher-Yates Algorithim for Shuffling
 const shuffle = (cards) => {
@@ -50,6 +58,8 @@ const delay = (ms) => {
 };
 
 const startRound = async () => {
+	createDeck();
+
 	// Shuffle cards
 	playingCards = shuffle(playingCards);
 
@@ -61,7 +71,16 @@ const startRound = async () => {
 
 	// Calculate Totals
 	calculateHandTotal(playerHand);
-	dealerScoreDiv.innerHTML = '<b>Total:</b> ???';
+
+	const { total } = calcTotal(dealerHand, true);
+
+	if (total == 21) {
+		dealerScoreDiv.innerHTML = '<b>Total:</b> 21';
+		endGame();
+		return;
+	} else {
+		dealerScoreDiv.innerHTML = '<b>Total:</b> ???';
+	}
 
 	// Show Action Buttons
 	buttonsDiv.setAttribute('class', 'row');
@@ -90,7 +109,7 @@ const deal = (cardIdx, toPlayer, faceDown) => {
 	cardIndex++;
 };
 
-const calculateHandTotal = (hand, toPlayer = true) => {
+const calcTotal = (hand, calcAce = false) => {
 	let total = 0;
 	let aceCount = 0;
 
@@ -98,7 +117,7 @@ const calculateHandTotal = (hand, toPlayer = true) => {
 	hand.forEach((card) => {
 		if (typeof card.card === 'string') {
 			if (card.card === 'A') {
-				total += 1;
+				total++;
 				aceCount++;
 			} else {
 				total += 10;
@@ -107,6 +126,22 @@ const calculateHandTotal = (hand, toPlayer = true) => {
 			total += card.card;
 		}
 	});
+
+	if (calcAce) {
+		for (let i = 0; i < aceCount; i++) {
+			let aceAddition = 10 * (i + 1);
+
+			if (total + aceAddition <= 21) {
+				total += aceAddition;
+			}
+		}
+	}
+
+	return { total, aceCount };
+};
+
+const calculateHandTotal = (hand, toPlayer = true) => {
+	const { total, aceCount } = calcTotal(hand);
 
 	let totalString = `${total}`;
 
@@ -131,18 +166,148 @@ const calculateHandTotal = (hand, toPlayer = true) => {
 const hitMe = async () => {
 	deal(cardIndex, true, false);
 	calculateHandTotal(playerHand);
+
+	const { total } = calcTotal(playerHand);
+	if (total > 21) {
+		return endGame();
+	}
 };
 
-const dealerTurn = () => {
-	const flippedCard = document.getElementById(3);
-	const card = playingCards[3];
-	flippedCard.innerHTML = `<b>${card.card} ${card.suit}</b>`;
-	calculateHandTotal(dealerHand, false);
+const dealerTurn = async (flipCard = true) => {
+	if (flipCard) {
+		const flippedCard = document.getElementById(3);
+		const card = playingCards[3];
+		flippedCard.innerHTML = `<b>${card.card} ${card.suit}</b>`;
+		calculateHandTotal(dealerHand, false);
+	}
 
-	// If 16 or less, hit. Otherwise stand.
+	await delay(1000);
+
+	const { total, aceCount } = calcTotal(dealerHand);
+
+	// Blackjack
+	if (total == 21) {
+		return endGame();
+	}
+
+	// Bust
+	if (total > 21) {
+		return endGame();
+	}
+
+	// Stand (check for soft 17 below)
+	if (total > 17) {
+		return endGame();
+	}
+
+	// No Aces Play
+	if (aceCount == 0) {
+		if (total < 17) {
+			return hitDealer();
+		} else {
+			return endGame();
+		}
+	}
+
+	// Aces Play
+	for (let i = aceCount; aceCount > -1; i--) {
+		let aceAddition = 10 * i;
+		let tempTotal = total + aceAddition;
+
+		// Stand or Win
+		if (tempTotal == 21 || (tempTotal > 17 && tempTotal < 21)) {
+			return endGame();
+		}
+
+		// Hit (Includes soft 17)
+		if (tempTotal <= 17) {
+			return hitDealer();
+		}
+
+		// Bust
+		if (i == 0 && tempTotal > 21) {
+			return endGame();
+		}
+	}
+};
+
+const hitDealer = async () => {
+	deal(cardIndex, false, false);
+	calculateHandTotal(dealerHand, false);
+	await dealerTurn(false);
+	return;
+};
+
+const endGame = () => {
+	const playerTotalObj = calcTotal(playerHand, true);
+	const dealerTotalObj = calcTotal(dealerHand, true);
+
+	let headerMessage = '';
+	let message = '';
+
+	if (playerTotalObj.total == dealerTotalObj.total) {
+		// Push
+		headerMessage = 'Push';
+		message = "Y'all tied";
+	} else if (dealerTotalObj.total == 21) {
+		// Dealer Blackjack
+		headerMessage = 'You lose';
+		message = 'Dealer Blackjack';
+	} else if (dealerTotalObj.total > 21) {
+		// Dealer Bust
+		headerMessage = 'You win';
+		message = 'Dealer Bust';
+	} else if (playerTotalObj.total == 21) {
+		// Player Blackjack
+		headerMessage = 'You win';
+		message = 'Player Blackjack';
+	} else if (playerTotalObj.total > 21) {
+		// Player Bust
+		headerMessage = 'You lose';
+		message = 'Player Bust';
+	} else if (playerTotalObj.total > dealerTotalObj.total) {
+		// Compare Totals
+		headerMessage = 'You win';
+		message = 'Player total beats Dealer';
+	} else if (dealerTotalObj.total > playerTotalObj.total) {
+		// Compare Totals
+		headerMessage = 'You lose';
+		message = 'Dealer total beats Player';
+	}
+
+	modalHeaderH1.innerText = headerMessage;
+	modalBodyDiv.innerText = message;
+	modal.show();
+
+	return;
+};
+
+const removeChildElements = (parent) => {
+	while (parent.firstChild) {
+		parent.removeChild(parent.lastChild);
+	}
+};
+
+const reset = async () => {
+	modal.hide();
+
+	cardIndex = 0;
+	playerHand = [];
+	dealerHand = [];
+
+	playerScoreDiv.innerHTML = '';
+	dealerScoreDiv.innerHTML = '';
+
+	removeChildElements(playerCardsDiv);
+	removeChildElements(dealerCardsDiv);
+
+	await delay(500);
+
+	return startRound();
 };
 
 hitButton.addEventListener('click', hitMe);
 standButton.addEventListener('click', dealerTurn);
+modalButton.addEventListener('click', reset);
 
 startRound();
